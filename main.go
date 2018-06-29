79c160b392571fafa7fbb447847c763a8f06cb66
Heungsub Lee <sub@subl.ee>
2018-06-30 01:31:06 +0900

    Allow all origins in CORS

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 )
 
 func init() {
 	router := gin.New()
+
+	// Allow all origins in CORS.
+	router.Use(cors.Default())
 
 	v2 := router.Group("/v2")
 	v2Init(v2)
