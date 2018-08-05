c6c08d99ed92bf5a4d65adf921fa6ec4e7d49472
Heungsub Lee <sub@subl.ee>
2018-08-06 01:06:48 +0900

    Make servicePronouncer

---
+++ default/pronouncer.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+)
+
+type servicePronouncer struct {
+	ctx context.Context
+	id  string
+}
+
+func (p *servicePronouncer) ID() string {
+	return p.id
+}
+
+func (p *servicePronouncer) Pronounce(word string) string {
+	hostname, err := appengine.ModuleHostname(p.ctx, p.id, "", "")
+	if err != nil {
+		log.Panicf("service hostname for %s not found", p.id)
+	}
+
+	url := fmt.Sprintf(
+		"http://%s/v2/pronounced/%s/%s",
+		hostname,
+		url.PathEscape(p.id),
+		url.PathEscape(word),
+	)
+
+	c := urlfetch.Client(p.ctx)
+	res, err := c.Get(url)
+	if err != nil {
+		log.Panicf("failed to get pronunciation of %s/%s: %s", p.id, word, err)
+	}
+
+	var buf bytes.Buffer
+	buf.ReadFrom(res.Body)
+	return buf.String()
+}
